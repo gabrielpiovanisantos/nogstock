02a46ad269ded35218b97c0bd724a74ae8b6b12b
gabriel piovani <gabrielpiovani@mangobits.com>
2020-05-28 15:10:45 -0300

    INITIAL COMMIT

---
+++ src/main/kotlin/com/gabriel/nogstock/NogstockApplication.kt
@@ -0,0 +1,11 @@
+package com.gabriel.nogstock
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NogstockApplication
+
+fun main(args: Array<String>) {
+    runApplication<NogstockApplication>(*args)
+}
